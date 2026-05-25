a5d3356a7d0a296fa1369278d01f41adeed173a9
Shridhar Bhat <141147285+shridhar618@users.noreply.github.com>
2026-05-25 17:12:07 +0530

    init

--- simplerouting/src/App.js
+++ simplerouting/src/App.js
@@ -1,33 +1,83 @@
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from 'react-router-dom';
+
+// // import './App.css';
+
+// import Navbar from './components/Navbar';
+// import Home from './components/Home';
+// import AboutUs from './components/AboutUs';
+// import Contact from './components/Contact';
+// import NoPage from './components/NoPage';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <BrowserRouter>
+//         <header className="App-header">
+//           <Navbar />
+//         </header>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/about" element={<AboutUs />} />
+//           <Route path="/contact" element={<Contact />} />
+//           <Route path="*" element={<NoPage />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+/*React Programmatic Navigation implements:
+1. useNavigate()
+2. Query String Routing
+3. Programmatic Navigation
+4. Dynamic URL update using dropdown
+Updated URL: http://localhost:3000/students?status=inactive
+*/
+import './App.css';
+import React from "react";
 import {
   BrowserRouter,
   Routes,
   Route,
-} from 'react-router-dom';
+  Link
+} from "react-router-dom";
 
-// import './App.css';
-
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import AboutUs from './components/AboutUs';
-import Contact from './components/Contact';
-import NoPage from './components/NoPage';
+import Home from "./components/Home";
+import StudentFilter from "./components/StudentFilter";
+import StudentList from "./components/StudentList";
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header"> </header>
       <BrowserRouter>
-        <header className="App-header">
-          <Navbar />
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
-      </BrowserRouter>
+      <h1>Student Records </h1>
+      <nav>
+        <Link to="/">Home</Link> |
+        <Link to="/students">Student Records</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/students"
+               element={
+                        <>
+                          <StudentFilter />
+                          <StudentList />
+                        </>
+                        }
+        />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
-
 export default App;
